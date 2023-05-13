526c053f8e61a7ca9e2dcad76231642456485dac
leesol <jbkljo@naver.com>
2023-05-13 12:01:13 +0900

    feat: 비밀번호 찾기  수정

--- src/Page/FindPw.js
+++ src/Page/FindPw.js
@@ -36,8 +36,8 @@ const Box = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 14px;
-  width: 606px;
-  height: 480px;
+  width: 653px;
+  height: 530px;
   border-radius: 30px;
   border: 1px solid #b5b5b5;
   font-size: 15px;
@@ -46,44 +46,61 @@ const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  & > div {
-    display: flex;
 
-    margin-top: 10px;
-    height: 68px;
-    font-weight: bold;
-  }
   & > input {
+    display: flex;
     width: 510px;
-    height: 41px;
+    height: 45px;
+    border-radius: 15px;
+    border: 1px solid #c5c5c5;
+    font-size: 20px;
+    padding-left: 20px;
   }
 `;
+const Name = styled.div`
+  color: #4f4747;
+  width: 100%;
+  display: flex;
+  margin-top: 49px;
+  margin-left: 15px;
+  font-size: 24px;
+  height: 68px;
+  font-weight: bold;
+  align-items: center;
+`;
 const Div1 = styled.div`
+  margin-top: 50px;
+  margin-bottom: 86px;
   width: 510px;
   display: flex;
   justify-content: space-between;
 `;
 const Div2 = styled.div`
-  /* margin-left: 11px;
-  margin-right: 11px; */
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  align-items: center;
   width: 242px;
   height: 243px;
 
   & > button {
-    height: 68px;
+    height: 46px;
+    width: 242px;
     border: none;
-    background-color: #f6e24b;
+    background-color: #007b59;
+    color: white;
+    border-radius: 30px;
+  }
+
+  & > img {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 195px;
+    height: 178px;
   }
 `;
-const IMG = styled.div`
-  height: 178px;
-  background-color: gray;
-  display: flex;
-  justify-content: space-between;
-`;
+
 const FindPw = () => {
   return (
     <Div>
@@ -95,15 +112,15 @@ const FindPw = () => {
         </Title2>
         <Box>
           <Content>
-            <div>새 비밀번호 설정</div>
+            <Name>새 비밀번호 설정</Name>
             <input placeholder="아이디를 입력해주세요"></input>
             <Div1>
               <Div2>
-                <IMG>휴대폰 이미지</IMG>
+                <img src="Image/phone.png" alt="phone" />
                 <button>휴대전화 인증</button>
               </Div2>
               <Div2>
-                <IMG>이메일 이미지</IMG>
+                <img src="Image/email.png" alt="phone" />
                 <button>이메일 인증</button>
               </Div2>
             </Div1>
