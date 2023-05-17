88e518d225de334dbc9e8598345372d1b0aa26bf
myejinni <bijou__ye@swu.ac.kr>
2023-05-17 13:03:03 +0900

    feat: 본인인증 페이지

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import './App.css';
 import React, {useEffect, useState} from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import axios from 'axios';
+import SelfAuth from "./Page/SignUp/SelfAuth";
+import InputInfo from "./Page/SignUp/InputInfo";
+import MemberAgree from "./Page/SignUp/MemberAgree";
+import MemberSuccess from "./Page/SignUp/MemberSuccess";
+
 
 function App() {
   const [hello, setHello] = useState('')
 
-   useEffect(() => {
-       axios.get('http://localhost:8080/api/hello')
-       .then(response => setHello(response.data))
-       .catch(error => console.log(error))
-   }, []);
+//    useEffect(() => {
+//        axios.get('http://localhost:8080/api/hello')
+//        .then(response => setHello(response.data))
+//        .catch(error => console.log(error))
+//    }, []);
 
    return (
-       <div>
-           백엔드에서 가져온 데이터입니다: {hello}
-       </div>
+    <div className="App">
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/oauth/kakao/callback" element={<Auth />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/findPw" element={<FindPw />}></Route> */}
+
+        <Route path="/selfAuth" element={<SelfAuth/>}></Route>
+        <Route path="/inputInfo" element={<InputInfo/>}></Route>
+        <Route path="/memberAgree" element={<MemberAgree/>}></Route>
+        <Route path="/memberSuccess" element={<MemberSuccess/>}></Route>
+
+      </Routes>
+    </BrowserRouter>
+  </div>
    );
 }
 
