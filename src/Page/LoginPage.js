d3e36312961fbb1a89290de048e90d7f0fd7bc65
leesol <jbkljo@naver.com>
2023-05-25 00:36:29 +0900

    feat:로그인 페이지 수정1

--- src/Page/LoginPage.js
+++ src/Page/LoginPage.js
@@ -102,11 +102,21 @@ const FindBtn = styled.button`
 `;
 const ProductBtn = styled.button`
   display: flex;
+  align-items: center;
+  justify-content: center;
   margin-top: 36px;
   margin-bottom: 4px;
   border-radius: 50%;
   width: 59px;
   height: 59px;
+  & > img {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40px;
+    height: 40px;
+    object-fit: contain;
+  }
 `;
 const KakaBtn = styled.button`
   width: 509px;
@@ -127,13 +137,26 @@ const SignBtn = styled.button`
   background-color: white;
 `;
 const AdverBtn = styled.button`
+  background-color: white;
+  border: none;
   margin-top: 63px;
   width: 506px;
   height: 243px;
+  padding: 0px;
+  & > img {
+    width: 506px;
+    height: 243px;
+  }
 `;
 const Bottom1 = styled.footer`
   background-color: #007b59;
   height: 514px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Bt = styled.div`
+  display: flex;
 `;
 const SubTitle = styled.h2`
   font-size: ${(props) => props.fontSize};
@@ -142,15 +165,25 @@ const SubTitle = styled.h2`
 `;
 const Div3 = styled.div`
   margin-top: 102px;
-  margin-left: 50px;
+  width: 1750px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `;
 const IMG = styled.div`
-  width: 362px;
-  height: 362px;
-  border: 1px solid black;
-  margin-right: 141px;
+  display: flex;
+  /* border: 1px solid black; */
+
+  & > img {
+    width: 350px;
+    height: 350px;
+  }
+  & > div {
+    font-size: 60px;
+    color: white;
+    margin-left: 20px;
+    margin-top: 150px;
+  }
 `;
 const LookBtn = styled.button`
   font-size: 20px;
@@ -162,14 +195,42 @@ const LookBtn = styled.button`
   color: white;
 `;
 const Bottom2 = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   background-color: #eae9e4;
-  height: 186px;
+  height: 304px;
   padding-left: 42px;
+`;
+const Div5 = styled.div`
+  width: 900px;
+`;
+const Div6 = styled.div`
+  margin-right: 60px;
+  display: flex;
+  & > div {
+    flex-direction: column;
+    display: flex;
+    align-items: end;
+    justify-content: end;
+    font-size: 28px;
+    width: 600px;
+    margin-right: 10px;
+  }
+  & > img {
+    width: 180px;
+    height: 180px;
+    object-fit: contain;
+  }
 `;
 const Div4 = styled.div`
   width: ${(props) => props.width};
   display: flex;
   justify-content: space-between;
+`;
+const H2 = styled.div`
+  font-size: 28px;
+  font-weight: ${(props) => props.fontWeight};
 `;
 const H3 = styled.h3`
   font-size: 24px;
@@ -223,16 +284,25 @@ const LoginPage = () => {
               </div>
               <div>
                 <div>
-                  <ProductBtn>카카오톡 사진</ProductBtn>카카오톡
+                  <ProductBtn>
+                    <img src="Image/kakao.png" alt="카카오톡 사진" />
+                  </ProductBtn>
+                  카카오톡
                 </div>
                 <div>
-                  <ProductBtn>네이버 사진</ProductBtn>네이버
+                  <ProductBtn>
+                    <img src="Image/naver.png" alt="네이버사진" />
+                  </ProductBtn>
+                  네이버
                 </div>
                 <div>
-                  <ProductBtn>네이버 사진</ProductBtn>네이버
+                  <ProductBtn>
+                    <img src="Image/facebook.png" alt="페이스북 사진" />
+                  </ProductBtn>
+                  페이스북
                 </div>
                 <div>
-                  <ProductBtn>휴대폰 사진</ProductBtn>휴대폰
+                  <ProductBtn></ProductBtn>휴대폰
                 </div>
               </div>
               <KakaBtn>
@@ -240,13 +310,15 @@ const LoginPage = () => {
               </KakaBtn>
               <SignBtn>회원가입</SignBtn>
 
-              <AdverBtn>광고자리</AdverBtn>
+              <AdverBtn>
+                <img src="Image/ad.png" alt="광고" />
+              </AdverBtn>
             </Div2>
           </div>
         </Box>
       </Main>
       <Bottom1>
-        <div>
+        <Bt>
           <Div3>
             <div>
               <SubTitle fontSize="32px" fontWeight="bold">
@@ -260,21 +332,41 @@ const LoginPage = () => {
               </SubTitle>
               <LookBtn>케리어 둘러보기</LookBtn>
             </div>
-            <IMG>이미지넣기</IMG>
+            <IMG>
+              <img src="Image/logo_white.png" />
+              <div>
+                <div>SUIT-</div>
+                <div>CARRIER</div>
+              </div>
+            </IMG>
           </Div3>
-        </div>
+        </Bt>
       </Bottom1>
       <Bottom2>
-        <Div4 width="70%">
-          <H3 fontWeight="bold">Contact Us</H3>
-          <H3 fontWeight="bold">gogoogle13@gamil.com</H3>
-          <H3 fontWeight="normal">서울시 관악구 봉천동 100-3 (캐리어빌딩)</H3>
-        </Div4>
-        <Div4 width="30%">
-          <H4>Tel | 010-1234-5678</H4>
-          <H4>Tel | 010-1234-5678</H4>
-        </Div4>
-        <H4>E-mail | suitcarrier_@naver.com</H4>
+        <Div5>
+          <Div4 width="1100px">
+            <H3 fontWeight="bold">Contact Us</H3>
+            <H3 fontWeight="bold">gogoogle13@gamil.com</H3>
+            <H3 fontWeight="normal">서울시 관악구 봉천동 100-3 (캐리어빌딩)</H3>
+          </Div4>
+          <Div4 width="500px">
+            <H4>Tel | 010-1234-5678</H4>
+            <H4>Tel | 010-1234-5678</H4>
+          </Div4>
+          <Div4>
+            <H4>E-mail | suitcarrier_@naver.com</H4>
+          </Div4>
+        </Div5>
+        <Div6>
+          <div>
+            <H2 fontWeight="bold">suit-carrier</H2>
+            <H2 fontWeight="nomal">
+              우리 집 창고 속 캐리어를, 우리 동네 캐리어로 !
+            </H2>
+          </div>
+
+          <img src="Image/logo.png" alt="logo" />
+        </Div6>
       </Bottom2>
     </Div>
   );
